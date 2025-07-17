e024020c7e396c5e3dda38701e9747b0c51b5992
hwanhy <hwanhy@gmail.com>
2025-07-17 20:14:23 +0900

    장바구니 기능 추가 재업로드

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,27 @@
 import 'package:flutter/material.dart';
-import 'package:today_s_farm/pages/add_product/add_product_page.dart';
+import 'package:provider/provider.dart';
+import 'package:today_s_farm/pages/cart/cart_page.dart';
 import 'package:today_s_farm/pages/home/home_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => CartProvider(),
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Today\'s Farm',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+        useMaterial3: true,
       ),
       home: const HomePage(),
     );
--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:today_s_farm/models/product_list.dart';
 import 'package:today_s_farm/models/product_model.dart';
 import 'package:today_s_farm/pages/add_product/add_product_page.dart';
+import 'package:today_s_farm/pages/cart/cart_page.dart';
 import 'package:today_s_farm/pages/detail/detail_page.dart';
 
 class HomePage extends StatefulWidget {
@@ -17,7 +18,21 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('상품 목록'), centerTitle: true),
+      appBar: AppBar(
+        title: const Text('상품 목록'),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.shopping_cart),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (_) => const CartPage()),
+              );
+            },
+          ),
+        ],
+      ),
       body: productList.isEmpty
           ? const Center(child: Text('상품이 없습니다.'))
           : ListView.builder(
@@ -29,12 +44,7 @@ class _HomePageState extends State<HomePage> {
                     Navigator.push(
                       context,
                       MaterialPageRoute(
-                        builder: (_) => DetailPage(
-                          title: product.name,
-                          description: product.description,
-                          imageUrl: product.imageUrl,
-                          price: product.price,
-                        ),
+                        builder: (_) => DetailPage(product: product),
                       ),
                     );
                   },
@@ -62,6 +72,10 @@ class _HomePageState extends State<HomePage> {
                                 : Image.asset(
                                     product.imageUrl!,
                                     fit: BoxFit.cover,
+                                    errorBuilder: (_, __, ___) => const Icon(
+                                      Icons.image,
+                                      color: Colors.white54,
+                                    ),
                                   ),
                           ),
                         ),
@@ -98,7 +112,6 @@ class _HomePageState extends State<HomePage> {
             MaterialPageRoute(builder: (_) => const AddProductPage()),
           );
 
-          // 등록 완료 후 새로운 상품을 목록 맨 위에 추가
           if (result != null && result is Product) {
             setState(() {
               productList.insert(0, result);
